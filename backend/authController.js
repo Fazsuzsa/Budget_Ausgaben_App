07e3d588bf56f75514da2f1e67c2ca1521aa7439
senda1988 <zidi.senda@gmail.com>
2025-05-12 10:23:02 +0200

    Create authController.js

---
+++ backend/authController.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET, JWT_REFRESH_SECRET } = process.env;
+function refreshToken(req, res) {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
+        return res.status(401).json({ error: "Refresh token is missing" });
+    }
+    {
+        if (err) {
+            return res.status(403).json({ error: "Invalid refresh token" });
+        }
+        const newAccessToken = jwt.sign({ userId: user.userId }, JWT_SECRET, {
+            expiresIn: '1d',
+        });
+        res.json({ accessToken: newAccessToken });
+    };
+}
+module.exports = { refreshToken };
