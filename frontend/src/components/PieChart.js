23eaff93be605020fc80792ef176edb50466c8a6
Nassima.amroun <nassima.amroun@tn.techstartere.de>
2025-05-14 23:57:46 +0200

    work on barchart frontend/backend

--- frontend/src/components/PieChart.js
+++ frontend/src/components/PieChart.js
@@ -9,6 +9,7 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend, Title } from "chart.js";
 import ChartDataLabels from "chartjs-plugin-datalabels"; // Import the plugin
 
 ChartJS.register(ArcElement, Tooltip, Legend, Title, ChartDataLabels);
+const token = localStorage.getItem("token");
 const options = {
   responsive: true,
   maintainAspectRatio: false,
